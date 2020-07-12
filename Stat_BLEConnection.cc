65fea44e7033143952a22ae564cdc876ab7236e9
Tham Nguyen <51147376+tha061@users.noreply.github.com>
2020-07-12 18:04:25 +1000

    Update Stat_BLEConnection.cc

--- Stat_BLEConnection.cc
+++ Stat_BLEConnection.cc
@@ -1,19 +1,8 @@
 /* -*- mode:c++ -*- ********************************************************
  * file:        Stat_BLEConnection.c
  *
-// Created on:  21.2.2014
- * Updated on:  21.2.2014
- * author:      Konstantin Mikhaylov
- *
- * copyright:   (C) 2014 Konstantin Mikhaylov
- *
- *              This program is free software; you can redistribute it
- *              and/or modify it under the terms of the GNU General Public
- *              License as published by the Free Software Foundation; either
- *              version 2 of the License, or (at your option) any later
- *              version.
- *              For further information see file COPYING
- *              in the top level directory
+ * copyright:   (C) 2014 CWC, University of Oulu, Finland
+ * Adding initial trust-aware implementation by Tham Nguyen, 2018.
  ***************************************************************************
  **************************************************************************/
 
