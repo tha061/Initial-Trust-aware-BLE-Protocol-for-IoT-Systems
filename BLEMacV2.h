eca5fcbd2cc373528ec3a10a3da04bbb0ecbac5c
Tham Nguyen <51147376+tha061@users.noreply.github.com>
2020-07-12 17:53:34 +1000

    Update BLEMacV2.h

--- BLEMacV2.h
+++ BLEMacV2.h
@@ -1,21 +1,9 @@
 /* -*- mode:c++ -*- ********************************************************
  // * file:        BLEMacV2.h
- // *
- // * Created on:  08.3.2014
- // * Updated on:  08.5.2014
- // * author:      Konstantin Mikhaylov
- // *
+ 
  // * copyright:   (C) 2014 CWC, University of Oulu, Finland
  // *
- // *              This program is free software; you can redistribute it
- // *              and/or modify it under the terms of the GNU General Public
- // *              License as published by the Free Software Foundation; either
- // *              version 2 of the License, or (at your option) any later
- // *              version.
- // ***************************************************************************
- // * OPEN ISSUES:
- // * 1) Transmit power setting
- // **************************************************************************/
+ // adding initial trust-aware implementation by Tham Nguyen, 2018.
 
 
 #ifndef BLEMacV2_H
