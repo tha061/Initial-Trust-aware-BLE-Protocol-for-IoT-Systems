cac9ea2ed77ca2903e2dac566ac4392931faeecb
Tham Nguyen <51147376+tha061@users.noreply.github.com>
2020-07-12 17:59:33 +1000

    Update BLE_LLControlInfo.h

--- BLE_LLControlInfo.h
+++ BLE_LLControlInfo.h
@@ -1,19 +1,8 @@
 /* -*- mode:c++ -*- *******************************************************
  * file:        BLE_LLControlInfo.h
  *
- * Created on:  15.02.2014
- * Updated on:  12.03.2014
- * author:      Konstantin Mikhaylov
- *
- * copyright:   (C) Konstantin Mikhaylov
- *
- *              This program is free software; you can redistribute it
- *              and/or modify it under the terms of the GNU General Public
- *              License as published by the Free Software Foundation; either
- *              version 2 of the License, or (at your option) any later
- *              version.
- *              For further information see file COPYING
- *              in the top level directory
+ * copyright:   (C) 2014 CWC, University of Oulu, Finland
+ * Adding initial trust-aware implementation by Tham Nguyen, 2018.
  **************************************************************************
  * part of:     framework implementation developed by tkn
  * description: - control info to pass the netw addresses between the
